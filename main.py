e457c59f456d119d0dfd2dc9c6cc71cd72acba09
robinvatshr08 <126511661+Rubeevatshr08@users.noreply.github.com>
2024-02-12 17:24:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run( os.getenv("BOT_TOKEN") )
